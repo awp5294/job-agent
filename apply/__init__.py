154b1723671c867d436da358d1727d7da7677bc4
awp5294 <awp5294@gmail.com>
2026-08-01 08:32:39 -0400

    add apply/__init__.py

---
+++ apply/__init__.py
@@ -0,0 +1,3 @@
+from .cover_letter import generate_cover_letter, stop_slop
+from .browser import prefill_application, open_job_for_review
+__all__ = ["generate_cover_letter", "stop_slop", "prefill_application", "open_job_for_review"]
